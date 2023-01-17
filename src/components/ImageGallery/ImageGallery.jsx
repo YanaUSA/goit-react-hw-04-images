fa8f6775e3b9749f5ec68aeeccd7f1bf46aa7899
YanaUSA <usatiukiana@gmail.com>
2023-01-17 14:36:15 +0200

    fixed fetch, add searchQuery to Gallery state

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 
@@ -10,63 +10,67 @@ import { LoadMoreBtn } from '../Button/Button';
 
 export const ImageGallery = ({ searchName }) => {
   const [hits, setHits] = useState([]);
+  const [searchQuery, setsearchQuery] = useState('');
   const [totalHits, setTotalHits] = useState(0);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
-  const fetchImages = () => {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const SearchParams = new URLSearchParams({
-      q: `${searchName}`,
-      page: `${pageNumber}`,
-      key: '31431099-cb6424a99d97f67db3bc0cdc7',
-      image_type: 'photo',
-      orientation: 'horizontal',
-      per_page: 12,
-    });
-
-    fetch(`${BASE_URL}?${SearchParams}`)
-      .then(res => res.json())
-      .then(({ hits, totalHits }) => {
-        if (!hits.length) {
-          toast.error(
-            `Oops, dear! Humanity hasn't invented such a word yet...`
-          );
-          return hits;
-        }
-
-        const fetchedData = hits.map(
-          ({ id, webformatURL, largeImageURL, tags }) => ({
-            id,
-            webformatURL,
-            largeImageURL,
-            tags,
-          })
-        );
-
-        setHits(prevHits => [...prevHits, ...fetchedData]);
-        setTotalHits(totalHits);
-      })
-      .catch(error => setError(error), setTotalHits(0))
-      .finally(() => setLoading(false));
-  };
-
   useEffect(() => {
-    if (searchName !== '') {
-      setHits([]);
-      setLoading(true);
-      setPageNumber(1);
-      fetchImages();
-    }
+    setHits([]);
+    // setTotalHits(0);
+    setPageNumber(1);
+    // setLoading(true);
+
+    setsearchQuery(searchName);
   }, [searchName]);
 
   useEffect(() => {
-    if (pageNumber > 1) {
-      setLoading(true);
-      fetchImages();
+    if (searchQuery === '') {
+      return;
     }
-  }, [pageNumber]);
+
+    setLoading(true);
+
+    const fetchImages = () => {
+      const BASE_URL = 'https://pixabay.com/api/';
+      const SearchParams = new URLSearchParams({
+        q: `${searchQuery}`,
+        page: `${pageNumber}`,
+        key: '31431099-cb6424a99d97f67db3bc0cdc7',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        per_page: 12,
+      });
+
+      fetch(`${BASE_URL}?${SearchParams}`)
+        .then(res => res.json())
+        .then(({ hits, totalHits }) => {
+          if (!hits.length) {
+            toast.error(
+              `Oops, dear! Humanity hasn't invented such a word yet...`
+            );
+            return hits;
+          }
+
+          const fetchedData = hits.map(
+            ({ id, webformatURL, largeImageURL, tags }) => ({
+              id,
+              webformatURL,
+              largeImageURL,
+              tags,
+            })
+          );
+
+          setHits(prevHits => [...prevHits, ...fetchedData]);
+          setTotalHits(totalHits);
+        })
+        .catch(error => setError(error), setTotalHits(0))
+        .finally(() => setLoading(false));
+    };
+
+    fetchImages();
+  }, [searchQuery, pageNumber]);
 
   const loadMore = () => {
     setPageNumber(prevPageNumber => prevPageNumber + 1);
